e213206ab5dba661adf3079e5bce0c5cfdeea25d
bruno <siqueirabruno3@gmail.com>
2024-10-18 15:15:56 -0300

    updating test files

--- cypress/e2e/fluxoCheckout.cy.js
+++ cypress/e2e/fluxoCheckout.cy.js
@@ -1,27 +1,28 @@
 describe('template spec', () => {
   describe('Efetuar compra no Luma', () => {
-    it('Validar seleção do produto e fazer checkout', () => {
-      cy.visit('https://magento.softwaretestingboard.com/');
+    beforeEach(() => {
       cy.login();
-
+    })
+    it('Validar seleção do produto e fazer checkout', () => {
+      cy.visit('/');
       cy.selecionarMenu();
       cy.escolherProduto();
       cy.get('.size').should('be.visible')
       cy.get('.swatch-attribute.color').should('be.visible')
 
-
-      cy.get('#option-label-size-143-item-167').click()
+      cy.selecionarCorTamanho();
       cy.get('#option-label-size-143-item-167').should('have.attr', 'aria-checked', 'true')
-
-      cy.get('#option-label-color-93-item-50').click()
       cy.get('#option-label-color-93-item-50').should('have.attr', 'aria-checked', 'true')
-
+    })
+    it('Validar checkout', () => {
+      cy.visit('/ajax-full-zip-sweatshirt.html');
+      cy.selecionarCorTamanho();
       cy.verificarProdutoNoCarrinho()
-      
+
       cy.get('.payment-method-content > :nth-child(4) > div.primary > .action').click();
 
       cy.get('.order-number > strong').should('be.visible');
       cy.get('.base').should('have.text', 'Thank you for your purchase!')
-    })
+    });
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,15 @@
 Cypress.Commands.add('login', () => {
-  cy.get('.panel > .header > .authorization-link > a').click();
-  cy.get('#email').type('desafiodot@gmail.com')
-  cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('A123456*@')
-  cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click();
+  cy.session('login', () => {
+    cy.visit('https://magento.softwaretestingboard.com/');
+    cy.get('.panel > .header > .authorization-link > a').click();
+    cy.get('#email').type('desafiodot@gmail.com')
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('A123456*@')
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click();
+  })
 })
 
 Cypress.Commands.add('selecionarMenu', () => {
-  cy.get('#ui-id-5').trigger('mouseover'); 
+  cy.get('#ui-id-5').trigger('mouseover');
   cy.get('#ui-id-17').trigger('mouseover');
   cy.get('#ui-id-20').click();
 })
@@ -27,8 +30,14 @@ Cypress.Commands.add('verificarProdutoNoCarrinho', () => {
   cy.wait('@prodNoCarrinho')
   cy.get('.message-success > div').should('be.visible')
   cy.get('.showcart').click()
-  cy.intercept('**/checkout/shipping_method/price.html**').as('resumoCarrinho')
+  cy.intercept('**/checkout/summary/item/price/**').as('resumoCarrinho')
   cy.get('#top-cart-btn-checkout').click()
+  cy.wait(2000)
   cy.wait('@resumoCarrinho')
   cy.get('.button').click()
+})
+
+Cypress.Commands.add('selecionarCorTamanho', () => {
+  cy.get('#option-label-size-143-item-167').click()
+  cy.get('#option-label-color-93-item-50').click()
 })
